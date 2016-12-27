ed5a5fc48cee085145333a457b2e82eb095a7c1a
Milen_Tanev <mil3ntanev@abv.bg>
2016-12-27 22:52:17 +0200

    navbar partially completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: [
+    './app.component.css',
+    '../../bower_components/bootstrap/dist/css/bootstrap.css'
+  ]
 })
 export class AppComponent {
-  title = 'app works!';
 }
