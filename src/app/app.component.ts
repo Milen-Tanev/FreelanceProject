187c2119abb9ebbe7bba49dd8d38411ea21d442a
Viliana Zhilkova <vilianazhilkova@abv.bg>
2016-12-28 17:46:29 +0200

    added bootstrap in styles.css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,8 +4,7 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: [
-    './app.component.css',
-    '../../bower_components/bootstrap/dist/css/bootstrap.css'
+    './app.component.css'
   ]
 })
 export class AppComponent {
