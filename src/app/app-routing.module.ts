f663aaeb9a9a41c7516113af498614be38200642
Viliana Zhilkova <vilianazhilkova@abv.bg>
2017-01-01 23:36:26 +0200

    Fixed bug in auth.guard.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,9 +6,9 @@ import { AppComponent } from './app.component';
 import { RegistrationComponent, LoginComponent } from './pages/users/index';
 import { FreelancersListComponent } from './pages/freelancers-list/freelancers-list.component';
 import { EmployersListComponent } from './pages/employers-list/employers-list.component';
-import { ListJobsComponent } from './jobs/job-list.component'
-import { HomeComponent } from './home/home.component'
-import { JobCreateComponent } from './jobs/job-create.component'
+import { ListJobsComponent } from './jobs/job-list.component';
+import { HomeComponent } from './home/home.component';
+import { JobCreateComponent } from './jobs/job-create.component';
 
 const routes: Routes = [
   {
@@ -30,7 +30,6 @@ const routes: Routes = [
   {
     path: 'user/profile',
     component: RegistrationComponent,
-    canActivate: [AuthGuard]
   },
   {
     path: 'jobs',
@@ -45,8 +44,10 @@ const routes: Routes = [
   },
   {
     path: 'create',
-    component: JobCreateComponent
-  }
+    component: JobCreateComponent,
+    canActivate: [AuthGuard]
+  },
+  { path: '**', redirectTo: '' }
 ];
 
 @NgModule({
--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,19 +1,20 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Rx';
 import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+
 @Injectable()
 export class AuthGuard implements CanActivate {
 
     constructor(private router: Router) { }
 
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-        if (sessionStorage.getItem('authtoken') !== null) {
+        if (sessionStorage.getItem('authtoken')) {
             // logged in so return true
             return true;
         }
 
         // not logged in so redirect to login page with the return url
-        this.router.navigate(['/']);
+        this.router.navigate(['/user/login'], { queryParams: { returnUrl: state.url }});
         return false;
     }
 }
